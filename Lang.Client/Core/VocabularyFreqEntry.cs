e02ba838e358ab234977956bd8f199dfddf31be3
Ozan Özten <goldenhawkk@gmail.com>
2022-10-29 19:37:53 +0300

    ref VocabularyFreqEntry

--- Lang.Client/Core/VocabularyFreqEntry.cs
+++ Lang.Client/Core/VocabularyFreqEntry.cs
@@ -1,11 +1,22 @@
-﻿namespace Lang.Client.Core;
+﻿using System.Text.Json.Serialization;
 
+namespace Lang.Client.Core;
+
+/// <summary>
+/// Represent frequency result of given word from vocabulary.com
+/// </summary>
 public class VocabularyFreqEntry
 {
-    public string word { get; set; }
+    [JsonPropertyName("word")]
+    public string Word { get; set; } = "";
+
+    [JsonPropertyName("freq")]
+    public float Freq { get; set; }
+
+    [JsonPropertyName("ffreq")]
+    public float Ffreq { get; set; }
+
     //public bool hw { get; set; }
-    public float freq { get; set; }
-    public float ffreq { get; set; }
     //public int size { get; set; }
     //public int type { get; set; }
     //public string parent { get; set; }
